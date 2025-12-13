7af232200f98edb55b35c0a7928626015bf41f8b
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-13 11:06:06 +0300

    hashPassword

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -8,3 +8,8 @@ const generateOTP = () => {
     return Math.floor(100000 + Math.random() * 900000).toString();
 };
 
+const hashPassword = async (password) => {
+    const saltRounds = 10;
+    return await bcrypt.hash(password,saltRounds);
+};
+
