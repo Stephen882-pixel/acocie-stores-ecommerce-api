f1886d5e305471f3ea78379b719d9d6b160aae85
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-13 11:14:24 +0300

    generate RefreshToken

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -16,3 +16,20 @@ const hashPassword = async (password) => {
 const comparePassword = async (password,hash) => {
     return await bcrypt.compare(password,hash);
 };
+
+const generateAccessToken = (userId, email,role) => {
+    return jwt.sign(
+        { userId, email, role },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_ACCESS_EXPIRY || '15m'}
+    );
+};
+
+const generateRefreshToken = (userId, email) => {
+    return jwt.sign(
+        { userId, email },
+        process.env.JWT_REFRESH_SECRET,
+        { expiresIn: process.env.JWT_REFRESH_EXPIRY || '7d' }
+    );
+};
+
