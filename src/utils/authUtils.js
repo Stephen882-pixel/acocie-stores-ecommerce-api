3b046ff95d20d34e4c3a8d3cffa9157465510a2c
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-13 11:07:34 +0300

    compare password

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -13,3 +13,6 @@ const hashPassword = async (password) => {
     return await bcrypt.hash(password,saltRounds);
 };
 
+const comparePassword = async (password,hash) => {
+    return await bcrypt.compare(password,hash);
+};
