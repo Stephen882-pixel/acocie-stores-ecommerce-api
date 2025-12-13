c441ea12b6c8a051f78521f1a35ee611d58c6990
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-13 21:31:46 +0300

    get profile controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,32 @@
 
+
+const { user, Address, LoginHistory } = require('../models');
+const { Op } = require('sequelize');
+
+const getProfile = async (req,res) => {
+    try{
+        const userId = req.user.userId;
+
+        const user = await user.findByPk(userId,{
+            attributes: { exclude: ['passwordHash'] },
+            include:[
+                {
+                    model:Address,
+                    as:'addresses'
+                }
+            ]
+        });
+
+        if(!user){
+            return res.status(404).json({error: 'User not found'});
+        }
+
+        res.json({ user })
+
+    } catch (error){
+        console.error('Error in getting profile:', error);
+        res.status(500).json({error:'Failed to fetch profile'});
+    }
+};
+
+
