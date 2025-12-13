318e9e05350eac1a12babb7bd6e24d5d8b227c4c
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-13 11:20:34 +0300

    is email valif

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -67,3 +67,8 @@ const getTokenExpiry = (expiryString) => {
     }
 };
 
+const isValidEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
