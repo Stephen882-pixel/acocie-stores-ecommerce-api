6a44486422890e233a01da621bb487ceb4393d52
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-14 21:09:47 +0300

    authorizeMinRole middlewaree

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -27,3 +27,26 @@ const authorize = (...allowedRoles) => {
   };
 };
 
+const authorizeMinRole =  (minRole) => {
+    return (req,res,next) => {
+        if(!req.user){
+            return res.status(401).json({
+                error:'Authentication required'
+            });
+        }
+
+        const userRole = req.user.role;
+        const userLevel = ROLE_HIERARCHY[userRole] || 0;
+        const minLevel = ROLE_HIERARCHY[[minRole]] || 0;
+
+        if(userLevel < minLevel){
+            return res.status(403).json({
+                error:'Access denied. Insufficient permisions',
+                minimumRole:minRole,
+                yourRole:userRole
+            });
+        }
+        next();
+    };
+};
+
