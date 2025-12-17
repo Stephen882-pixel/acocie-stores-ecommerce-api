17b3349a1407f440ebe6afb4e1fc9cc2b7d5e25b
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-17 09:42:35 +0300

    check product existance

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -335,6 +335,16 @@ const updateProduct = async (req,res) => {
 
 const deleteProduct = async (req,res) => {
     try{
+        const { id } = req.params;
+
+        const product = await Product.findByPk(id);
+
+        if(!product){
+            return res.status(404).json({
+                error:'Product not found'
+            });
+        }
+
         
     } catch (error){
         console.error('Error in deleteProduct:',error);
