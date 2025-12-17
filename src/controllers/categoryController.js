d85e428ecba511948271181e9278259e47c5b270
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-17 10:25:37 +0300

    validate if parent category is provided

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,6 +1,37 @@
+const { Category } = require("../models");
 
 const createCategory = async (req,res) => {
     try{
+        const {
+            name,
+            description,
+            parentId,
+            imageUrl,
+            isActive,
+            displayOrder,
+            metaTitle,
+            metaDescription
+        } = req.body;
+
+        if(!name){
+            return res.status(400).json({error:'Category name is requied'});
+        }
+
+        const slug = generateSlug(name);
+        let finalSlug = slug;
+        let counter = 1;
+
+        while(await Category.findOne({where:{ slug:finalSlug }})){
+            finalSlug =  `${slug}-${counter}`;
+            counter++;
+        }
+
+        if(parentId){
+            const parent = await Category.findByPk(parentId);
+            if(!parent){
+                return res.status(404).json({error:'Parent category not found'});
+            }
+        }
         
     } catch (error){
         console.error('Error in createCategory:',error);
