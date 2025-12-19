94cba4df5908cab6ce800eef5e34ef5a4d24776d
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-19 15:17:03 +0300

    helper method to record status change

---
+++ src/controllers/adminOrderController.js
@@ -0,0 +1,27 @@
+const {
+  Order,
+  OrderItem,
+  OrderStatusHistory,
+  OrderTracking,
+  OrderCancellation,
+  OrderNote,
+  Address,
+  Product,
+  ProductVariant,
+  Inventory,
+  User,
+  sequelize
+} = require('../models');
+const { Op } = require('sequelize');
+const emailService = require('../services/emailService');
+
+
+const recordStatusChange = async (orderId, oldStatus, newStatus, userId, reason = null) => {
+  await OrderStatusHistory.create({
+    orderId,
+    oldStatus,
+    newStatus,
+    changedByUserId: userId,
+    changeReason: reason
+  });
+};
