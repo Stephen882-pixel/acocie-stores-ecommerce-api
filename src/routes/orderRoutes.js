03286fc81730ab120306eb01d6d10fe842c68b07
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-22 15:42:11 +0300

    order routes

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController');
+const authenticate = require('../middleware/authMiddleware');
+
+router.use(authenticate);
+
+router.get('/',orderController.getOrderHistory);
+router.get('/stats',orderController.getOrderStats);
+router.get('/number/:orderNumber',orderController.getOrderByNumber);
+router.get('/:id',orderController.getOrderById);
+router.get('/:id/tracking',orderController.getOrderTracking);
+router.post('/:id/cancel',orderController.requestOrderCancellation);
+router.post('/:id/return',orderController.requestReturn);
+router.post('/:id/notes',orderController.addCustomerNote);
+module.exports = router;
